ca2f9e3340b139f62cc12d0ef72ae92c60370303
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-08-07 21:23:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,10 @@ class Song
   attr_accessor :title, :artist
 end
 
-imagine_dragons = Artist.new
-imagine_dragons.name = "Imagine Dragons"
-radioactive = Song.new
-radioactive.artist = imagine_dragons
+# imagine_dragons = Artist.new
+# imagine_dragons.name = "Imagine Dragons"
+# radioactive = Song.new
+# radioactive.artist = imagine_dragons
 
 radioactive.artist.name
 # => "Imagine Dragons"
