c3f91394ab3433eb7f6a3aa2d5c80cf8a7a877db
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-08-07 21:31:17 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,5 +2,11 @@ class Post
   attr_accessor :title, :author
 end
 
-jonathan = Author.new
-jonathan.name = "Jonathan"
+# jonathan = Author.new
+# jonathan.name = "Jonathan"
+#
+# 1st_post = Post.new
+# 1st_post.author = jonathan
+#
+# 1st_post.author.name
+# => "Jonathan"
